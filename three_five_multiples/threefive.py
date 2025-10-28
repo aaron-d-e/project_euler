3517748835c3ff6c8bf590979a632fee8bdb6cba
Aaron Evans <aarone0621@gmail.com>
2025-10-27 22:05:07 -0500

    solved multiple of three or five - problem 1

--- three_five_multiples/threefive.py
+++ three_five_multiples/threefive.py
@@ -1,2 +1,16 @@
 def threefive(value):
+    count = 0
+    ans = 0
+    while count < value:
+        if count % 3 == 0 or count % 5 == 0:
+            ans = ans + count
+
+        count = count + 1
+
+    return ans
+
+
+x = threefive(1000)
+print(x)
+
 
